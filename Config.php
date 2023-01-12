f989f7db6765a27ee7ac505760139172d17f87e1
alirezahms <64715752+alirezahms@users.noreply.github.com>
2023-01-12 19:06:55 +0330

    Add files via upload

---
+++ Config.php
@@ -0,0 +1,7 @@
+<?php
+
+const Config = [
+    'Admin'      => [277826937,5570475400],
+    'AppInfo'    => ['Api_Id'=> '11567492','Api_Hash'=> '9b857b5f837f6d7f6d6d1ecfb46cfe57'],
+    'Database'   => ['User'=>'tadhihsu_resid','Name'=>'tadhihsu_resid','Password'=>'tadhihsu_resid']
+];
